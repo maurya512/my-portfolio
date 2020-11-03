ab4362d58281662f8b02d7235c6274c28d884c07
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-03 09:36:59 -0500

    changed some components

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,10 @@
-import React, { Component } from 'react';
-import Sidebar from './components/sidebar';
-import Introduction from './components/introduction';
-import About from './components/about';
-import Timeline from './components/timeline';
 
+// import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div id="colorlib-page">
-        <div id="container-wrap">
-          <Sidebar></Sidebar>
-          <div id="colorlib-main">
-            <Introduction></Introduction>
-            <About></About>
-            <Timeline></Timeline>
-          </div>
-        </div>
-      </div>
-    )
   }
 }
 
